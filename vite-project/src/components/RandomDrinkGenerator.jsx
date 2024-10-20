65edf506d982a677d78e622974d83f1c5b951d31
Mason <chtuff@gmail.com>
2024-10-20 12:52:55 -0400

    react was not reading array index property as a stand alone statement had to wrap it in a map method, still needs work

--- vite-project/src/components/RandomDrinkGenerator.jsx
+++ vite-project/src/components/RandomDrinkGenerator.jsx
@@ -1,38 +1,50 @@
 import { useState, useEffect } from "react";
-import { Children } from "react";
 export default function RandomDrinkGenerator() {
-    const result = []
     let [data, setData] = useState([])
     useEffect(() => {
                 const fetchData = async () => {
                     try {
                         const response = await fetch ('https://www.thecocktaildb.com/api/json/v1/1/random.php')
                         const jsonData = await response.json()
-                        return jsonData
+                        return setData(jsonData) 
                         
                     }catch (error){
                         console.error('Error fetching data:', error)
                     }
                 };
          
-                fetchData().then(setData)
-    },[])
+                fetchData();
+            },[])
         console.log(data)
         return (
             <div>
-                
-                        <div>
-                            <h3>{data?.drinks['0']?.strDrink}</h3>
-                            <img src={data?.drinks['0']?.strDrinkThumb}/>  
-                        </div>    
-                        <div>
-                             <ul>
+                            
+                        {Object.values(data).map((value, index) =>  (
+                        <h3 key={index}>{value = data?.drinks[index]?.strDrink} </h3> 
+                        ))}
+                        {Object.values(data).map((value, index) =>  (
+                        <img src={value = data?.drinks[index]?.strDrinkThumb} /> 
+                        ))}
+                        {Object.values(data).map((child, index) =>  (
+                        <ul>
+                            <li key={index}>{data?.drinks[index]?.strIngredient1}</li> 
+                            <li key={index}>{data?.drinks[index]?.strIngredient2}</li> 
+                            <li key={index}>{data?.drinks[index]?.strIngredient3}</li> 
+                            <li key={index}>{data?.drinks[index]?.strIngredient4}</li> 
+                        </ul>  
+                        ))}
+                             
+                            {/* <h3>{data?.drinks?.strDrink}</h3>
+                            <img src={data?.drinks?.strDrinkThumb}/>  
+                            </div>     */}
+                        {/* <div>
+                              <ul>
                                 <li>{data?.drinks['0']?.strIngredient1}</li> 
                                 <li>{data?.drinks['0']?.strIngredient2}</li> 
                                 <li>{data?.drinks['0']?.strIngredient3}</li> 
                                 <li>{data?.drinks['0']?.strIngredient4}</li> 
-                             </ul>
-                        </div>
+                             </ul> 
+                        </div> */}
                 
             </div>
         )
